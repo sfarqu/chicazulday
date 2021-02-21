b397ae5ba70c71071dbec75950e6dee80bf86c0d
Sara Farquharson <51892187+sfarqu@users.noreply.github.com>
2021-02-20 23:42:48 -0800

    Add descriptions and aesthetics
    - add overall description
    - add remaining event descriptions
    - replace ugly bulleted list with flex box & links

--- src/App.js
+++ src/App.js
@@ -13,18 +13,28 @@ function App() {
         <div className={"App-body"}>
             <h1>Livestream</h1>
             <a href={"https://www.twitch.tv/fabrefact"}>Twitch Stream</a>
+            <h1>What is this?</h1>
+            <div className={"container"}>
+                <p className={"textBlock"}>Chicazul Day is a time to celebrate through acts of kindness to yourself and those around you.
+                    Every year on this day I donate to the Greater Vancouver Food Bank—this year I am asking you to donate with me,
+                    either to my charity or your favourite charity in your own community.</p>
+            </div>
+
             <h1>Special Guests</h1>
-            <ul>
-                <li>Mary Robinette Kowal</li>
-                <li>Dr. Sarah Ficke</li>
-                <li>Sarah Donner & Kittens Slay Dragons</li>
-                <li>Molly Lewis</li>
-                <li>Andrew Ferguson</li>
-                <li>Pat Rothfuss</li>
-                <li>Dana Pellebon</li>
-                <li>Em Lazer-Walker</li>
-                <li>Coriander Dickenson</li>
-            </ul>
+            <div className={"container"}>
+                <ul className={"list"}>
+                    <li className={"item"}><a href={"https://twitter.com/MaryRobinette"}>Mary Robinette Kowal</a></li>
+                    <li className={"item"}><a href={"https://twitter.com/DameMystery"}>Dr. Sarah Ficke</a></li>
+                    <li className={"item"}><a href={"https://twitter.com/sarahdonner"}>Sarah Donner</a></li>
+                    <li className={"item"}><a href={"https://twitter.com/meowslaydragons"}>Kittens Slay Dragons</a></li>
+                    <li className={"item"}><a href={"https://twitter.com/Molly23"}>Molly Lewis</a></li>
+                    <li className={"item"}><a href={"https://twitter.com/warandpeace"}>Andrew Ferguson</a></li>
+                    <li className={"item"}><a href={"https://twitter.com/PatrickRothfuss"}>Pat Rothfuss</a></li>
+                    <li className={"item"}><a href={"#"}>Dana Pellebon</a></li>
+                    <li className={"item"}><a href={"https://twitter.com/lazerwalker"}>Em Lazer-Walker</a></li>
+                    <li className={"item"}><a href={"https://twitter.com/apsalar"}>Coriander Dickinson</a></li>
+                </ul>
+            </div>
             <h1>Schedule</h1>
             <p>All times in Pacific time (UTC -8:00)</p>
             <Timeline lineColor={'#ddd'} className={"App-body"}>
@@ -35,7 +45,7 @@ function App() {
                     bodyContainerStyle={{ color: 'white' }}
                        >
                     <h3>Stream kickoff!</h3>
-                    <p>Explaining what we're doing here</p>
+                    <p>An explanation of what this all is and maybe a few unscheduled guests!</p>
                     </TimelineItem>
                 <TimelineItem dateText={"12:30pm-2:00pm"}
                               dateInnerStyle={{ background: '#00c4cc' }}
@@ -51,7 +61,7 @@ function App() {
                 >
                     <h3>How to pick a charity to support</h3>
                     <h4>Pat Rothfuss, Dana Pellebon</h4>
-                    <p></p>
+                    <p>Worldbuilders board members talk about how to choose where to put your charitable dollars.</p>
                 </TimelineItem>
                 <TimelineItem dateText={"3:00pm-4:00pm"}
                               dateInnerStyle={{ background: '#00c4cc' }}
@@ -59,7 +69,7 @@ function App() {
                 >
                     <h3>Designing virtual social spaces</h3>
                     <h4>Em Lazer-Walker</h4>
-                    <p></p>
+                    <p>Talking about how to create meaningful social interactions in virtual spaces, and showing off some spaces we've made.</p>
                 </TimelineItem>
                 <TimelineItem dateText={"4:00pm-5:00pm"}
                               dateInnerStyle={{ background: '#00c4cc' }}
@@ -67,7 +77,7 @@ function App() {
                 >
                     <h3>Music time!</h3>
                     <h4>Sarah Donner, Kittens Slay Dragons, Molly Lewis</h4>
-                    <p></p>
+                    <p>Excellent musical friends playing excellent music!</p>
                 </TimelineItem>
                 <TimelineItem dateText={"5:00pm-6:00pm"}
                               dateInnerStyle={{ background: '#00c4cc' }}
@@ -75,7 +85,7 @@ function App() {
                 >
                     <h3>A tour of VR games I've made</h3>
                     <h4>Molly Lewis, Andrew Ferguson, Coriander Dickenson</h4>
-                    <p></p>
+                    <p>Check out Hat Island and a giant robot in space with my game jam co-creators and host Molly Lewis</p>
                 </TimelineItem>
             </Timeline>
         </div>
